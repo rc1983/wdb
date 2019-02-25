18aff29f5dccd71dbbaeced2514bb3f73013cd6e
rc1983 <rch@fastmail.com>
2019-02-25 07:45:17 -0600

    js exercises

--- 08-javascript/js-stalker/stalker.js
+++ 08-javascript/js-stalker/stalker.js
@@ -1,5 +1,6 @@
-var firstName = prompt("Please enter your first name: ");
-var lastName = prompt("Please enter your last name: ");
-var age = prompt("Enter your age.");
-alert("The first name is: " + firstName + ". \nThe last name is: " + lastName + ".\nThe age is: " + age);
-console.log("The first name is: " + firstName + ". \nThe last name is: " + lastName + ".\nThe age is: " + age);
+var firstName = prompt("Enter the first name:");
+var lastNAme = prompt("Enter the last name:");
+var fullName = firstName + " " + lastNAme;
+console.log("Name: " + fullName);
+var age = prompt("Enter the age: ");
+console.log(fullName + " is " + age + " year(s) old.");
--- 08-javascript/stalker.js
+++
@@ -1,6 +0,0 @@
-var firstName = prompt("Enter the first name:");
-var lastNAme = prompt("Enter the last name:");
-var fullName = firstName + " " + lastNAme;
-console.log("Name: " + fullName);
-var age = prompt("Enter the age: ");
-console.log(fullName + " is " + age + " year(s) old.");
