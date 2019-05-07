cf395fe70edee6c6d28429c23d5f58d046e3e48a
rc1983 <rch@fastmail.com>
2019-05-06 22:28:43 -0500

    more functions

--- 12-functions/function-dec-exp.js
+++ 12-functions/function-dec-exp.js
@@ -7,7 +7,7 @@ function getFormData()
         if (choice[i].checked)
         {
             console.log("Choice is: " + choice[i].value);
-            runQuestion(choice[i].value);
+            document.getElementById("ret").innerHTML = runQuestion(choice[i].value);
         }
     }
 }
@@ -30,13 +30,13 @@ function runQuestion(choice)
                 return 8;
             }
         }
-        console.log("Output: " + foo());
+        return ("Output: " + foo());
     }
 
     else if (choice == 2)
     {
         console.log("Running the function for Question 2");
-        
+
         function foo()
         {
             var bar = function ()
@@ -49,14 +49,14 @@ function runQuestion(choice)
                 return 8;
             };
         }
-        console.log("Output: " + foo());
+        return ("Output: " + foo());
     }
 
     else if (choice == 3) 
     {
         console.log("Running the function for Question 3");
 
-        console.log("Output: " + foo());
+        return ("Output: " + foo());
         function foo()
         {
             var bar = function ()
@@ -64,29 +64,36 @@ function runQuestion(choice)
                 return 3;
             };
             return bar();
-            var bar = function ()
-            {
-                return 8;
-            };
-        }        
-    }
-
-    else
-    {
-        console.log("Running the function for Question 4");
-        function foo()
-        {
-            return bar();
-            var bar = function ()
-            {
-                return 3;
-            };
             var bar = function ()
             {
                 return 8;
             };
         }
-        console.log("Output: " + foo());
+    }
+
+    else
+    {
+        try
+        {
+            console.log("Running the function for Question 4");
+            function foo()
+            {
+                return bar();
+                var bar = function ()
+                {
+                    return 3;
+                };
+                var bar = function ()
+                {
+                    return 8;
+                };
+            }
+            return ("Output: " + foo());
+        } 
+        catch(e)
+        {
+            return(e.message + "\n\r" + e.stack);
+        }
     }
 }
 // question 1
