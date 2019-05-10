cdcfde506ae88eb678f3064a3e47068324a6c5ab
ravi <ravi@powercosts.com>
2019-05-10 16:02:08 -0500

    javascript problem set

--- 12-functions/function-problem-set.js
+++ 12-functions/function-problem-set.js
@@ -17,3 +17,36 @@ function evenNumberTest()
         document.getElementById("evenNumberTest").innerHTML = num + " is odd.";
     }
 }
+
+// function fac()
+// {
+//     var input = document.getElementById("fac");
+
+//     var num = Number(input.value);
+
+
+
+//     if (!(num == "NaN"))
+//     {
+//         document.getElementById("fac").innerHTML = "The factorial is: " + Math.;
+//     }
+//     else
+//     {
+//         document.getElementById("fac").innerHTML = input.value + " is not a number";
+//     }
+// }
+
+getFactorial(3)
+
+function getFactorial(num)
+{
+    if (num == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        console.log(num);
+        console.log(num * (getFactorial(num-1)));
+    }
+}
