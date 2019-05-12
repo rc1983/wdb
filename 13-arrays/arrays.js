2726891f1ab0649fff24b2d44b9d9530c0f97770
rc1983 <rch@fastmail.com>
2019-05-11 19:40:28 -0500

    arrays and todolist

---
+++ 13-arrays/arrays.js
@@ -0,0 +1,46 @@
+var friends = ["a", "b", "c", "d"];
+var colors = ["red", "orange", "yellow"];
+colors[3] = "green";
+console.log("colors: " + colors);
+colors[3] = "dark green";
+console.log("colors: " + colors);
+colors[6] = "yellow";
+colors[10] = "violet";
+console.log("colors = " + colors);
+console.log("length of colors: " + colors.length);
+
+// array methods
+
+console.log("before push colors: " + colors);
+//push also returns the length of the array
+var length = colors.push("indigo");
+console.log("length = " + length);
+console.log("colors after push: " + colors);
+
+console.log("colors before pop: " + colors);
+var pop = colors.pop();
+// pop returns the last item in the array that is remoted
+console.log("colors after pop: " + colors);
+console.log("pop: " + pop);
+
+// unshift adds an item to the beginning of an array
+console.log("before shift: " + colors);
+var unshift = colors.unshift("neon");
+console.log("after unshift: " + colors);
+console.log("unshift: " + unshift);
+
+//shift removes an element from the beginning
+console.log("before shift: " + colors);
+var shift = colors.shift();
+console.log("shift: " + shift);
+console.log("after shift: " + colors);
+
+//indexOf method
+console.log("index of orange: " + colors.indexOf("orange"));
+// when there are duplicate elements, the first index that is found is returned
+console.log("index of yellow: " + colors.indexOf("yellow"));
+
+//slice
+var fruits = ["banana", "orange", "lemon", "apple", "mango"];
+var citrus = fruits.slice(1, 3); //this would include elements 1, 2
+console.log("citrus: " + citrus);
---
+++ 13-arrays/to-do-list/ToDoList.js
@@ -0,0 +1,28 @@
+function beginToDoList() 
+{
+    var toDoList = [];
+
+    var input = prompt("What would you like to do?");
+
+    while (input !== "quit") 
+    {
+        if (input === "new") 
+        {
+            var newToDo = prompt("Enter a new to do item: ");
+            toDoList.push(newToDo);
+            input = prompt("What would you like to do?");
+        }
+        else if (input === "list") 
+        {
+            console.log("To do list: " + toDoList);
+            document.getElementById("list").innerHTML = toDoList;
+            input = prompt("What would you like to do?");
+        }
+        else
+        {
+            console.log("Unrecognized input. Try the following inputs - new, list, quit");
+            input = prompt("What would you like to do?");
+        }
+    }
+    console.log("Quit To Do List");
+}
