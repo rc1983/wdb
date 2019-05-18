c362c2e2882e6fb626d3296c8634bd4ebea7bf12
rc1983 <rch@fastmail.com>
2019-05-18 18:23:17 -0500

    array iteration

--- 13-arrays/ArrayIteration.js
+++ 13-arrays/ArrayIteration.js
@@ -1,3 +1,6 @@
+// this method is used because javascript file loads at the beginning, and this would
+// cause innerHTML of null error
+// https://stackoverflow.com/questions/18239430/cannot-set-property-innerhtml-of-null
 window.onload = function()
 {
     var colors = ["red", "green", "blue"];
@@ -25,4 +28,16 @@ window.onload = function()
         console.log("***********");
     }
     colors.forEach(printColors);
+    colors.forEach(function(el, i, arr)
+    {
+        console.log("el: " + el + ", i: " + i + ", arr: " + arr);
+    });
+    // second example
+    console.log("The following output is from a function that is being called by forEach");
+    function logElements(el, i, arr)
+    {
+
+        console.log("el: " + el + ", i: " + i + ", arr: " + arr);
+    }
+    colors.forEach(logElements);
 }
