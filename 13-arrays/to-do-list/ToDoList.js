89db9204b498f565d0ff36d4c7c230ced6928252
rch@fastmail.com <rch@fastmail.com>
2019-05-26 12:52:19 -0700

    array to do list refactor

--- 13-arrays/to-do-list/ToDoList.js
+++ 13-arrays/to-do-list/ToDoList.js
@@ -1,28 +1,73 @@
+var toDoList = [];
+
 function beginToDoList() 
 {
-    var toDoList = [];
-
-    var input = prompt("What would you like to do?");
+    var input = prompt("What would you like to do? Type: \"quit\" to exit");
 
     while (input !== "quit") 
     {
         if (input === "new") 
         {
-            var newToDo = prompt("Enter a new to do item: ");
-            toDoList.push(newToDo);
-            input = prompt("What would you like to do?");
+            addNewElement();
         }
         else if (input === "list") 
         {
-            console.log("To do list: " + toDoList);
-            document.getElementById("list").innerHTML = "To do list: " + toDoList;
-            input = prompt("What would you like to do?");
+            listToDos();
+        }
+        else if (input === "delete")
+        {
+            deleteToDo();            
         }
         else
         {
-            console.log("Unrecognized input. Try the following inputs - new, list, quit");
-            input = prompt("What would you like to do?");
+            console.log("Unrecognized input. Try the following inputs - new, list, quit, delete");
+            alert("Unrecognized input. Try the following inputs - new, list, quit, delete");
         }
+        input = prompt("What would you like to do?");
     }
     console.log("Quit To Do List");
+    document.getElementById("list").innerHTML = "Quit To Do List";
+}
+
+function addNewElement()
+{
+    do 
+    {
+        newToDo = prompt("Enter a new to do item (Enter done to exit): ");
+        if(newToDo !== "done")
+        {
+            toDoList.push(newToDo);
+            console.log("Added: " + newToDo);
+        }
+    }
+    while (newToDo !== "done")
+}
+
+function listToDos()
+{
+    console.log("*****");
+    toDoList.forEach(todo);
+    function todo(element, index)
+    {
+        console.log(index + ": " + element);
+    }
+    console.log("*****");
+    console.log("To do list: " + toDoList);
+    document.getElementById("list").innerHTML = "To do list: " + toDoList;
+}
+
+function deleteToDo()
+{
+    var index = Number(prompt("Enter the index to delete."));
+    if(!index=="NaN" && (index < toDoList.length))
+    {
+        console.log("Deleting the following element: " + toDoList[index]);
+        // index of the element you want to delete, and the number elements to delete. 
+        // to delete only one element, use 1
+        toDoList.splice(index,1)
+    }
+    else
+    {
+        console.log("Invalid input: " + index);
+    }
 }
